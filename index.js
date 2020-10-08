9fc9387266d1f035634542ee5b54139237745021
an_tosha33 <an_tosha33@mail.ru>
2020-10-08 13:26:03 +0300

    feat: add checker

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
-// const dns = require('dns');
+const { Resolver } = require('dns').promises;
+const resolver = new Resolver();
+const Telnet = require('telnet-client');
+const fs = require('fs');
+module.exports = class EmailChecker {
 
-// dns.resolveMx('gmail.com', (err, mess) => {
-//   console.log(err, mess);
-// })
-
-const Telnet  = require('telnet-client');
-
-async function run() {
-  let connection = new Telnet()
-
-  // these parameters are just examples and most probably won't work for your use-case.
-  let params = {
-    // host: 'gmail-smtp-in.l.google.com',
-    host: 'mxs.mail.ru',
-    port: 25,
-    shellPrompt: / .+/, // or negotiationMandatory: false
-    timeout: 1500
+  constructor(mailServers) {
+    this.servers = mailServers;
+    this.mailHosts = {};
+    this.getMailHosts();
+    this.telnet = new Telnet();
   }
 
-  try {
-    await connection.connect(params)
-  } catch(error) {
-    // handle the throw (timeout)
+  getMailHosts() {
+    this.servers.forEach((mailServer) => {
+      this.mailHosts[mailServer] = async () => await resolver.resolveMx(mailServer);
+    })
+    return this.mailHosts;
   }
 
-  let res = await connection.send('helo test.com')
-  res = await connection.send('mail from: <me@example.com>')
-  res = await connection.send('rcpt to: <an_tosha33@mail.ru>')
-  res = await connection.send('data')
-  res = await connection.send('.')
-  console.log('async result:', res)
+  async checkEmail({
+    email,
+    host,
+    port = 25
+  }) {
+
+    const regExp = new RegExp(`(?<=@).*`);
+    const domain = email.match(regExp)[0];
+
+
+    if (!host) {
+      const res = await this.mailHosts[domain]();
+      host = res[0].exchange;
+    }
+
+    try {
+      await this.telnet.connect({
+        host,
+        port,
+        shellPrompt: /.+/,
+        timeout: 1500
+      })
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+
+    try {
+      let res;
+      res = await this.telnet.send('helo example.com')
+      res = await this.telnet.send(`mail from: <me@${domain}>`);
+      res = await this.telnet.send(`rcpt to: <${email}>`);
+      switch (domain) {
+        case 'mail.ru':
+          res = await this.telnet.send('data');
+          res = await this.telnet.send('.')
+          if (res.search('invalid mailbox') !== -1) {
+            this.telnet.end();
+            return false;
+          } else {
+            this.telnet.end();
+            return true;
+          }
+        case 'yandex.ru':
+          if (res.search('550') !== -1) {
+            await this.telnet.end();
+            return false;
+          } else {
+            await this.telnet.end();
+            return true;
+          }
+        case 'gmail.com':
+          if (res.search('550') !== -1) {
+            await this.telnet.end();
+            return false;
+          } else {
+            await this.telnet.end();
+            return true;
+          }
+      }
+    } catch (e) {
+      return e;
+    }
+  }
 }
-
-run()
